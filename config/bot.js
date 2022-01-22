9ee50d5efde2213d79a07e408e0221ab6f872cdf
EmirGencer <79666042+EmirGencer@users.noreply.github.com>
2022-01-22 23:26:06 +0300

    Add files via upload

---
+++ config/bot.js
@@ -0,0 +1,17 @@
+module.exports = {
+    emojis: {
+        off: ':x:',
+        error: ':warning:',
+        queue: ':bar_chart:',
+        music: ':musical_note:',
+        success: ':white_check_mark:',
+    },
+
+    discord: {
+        token: 'OTM0NTE4MDA2Mzc2MTg1ODk3.YexPtw.75OcvvLU98xi4hDLK6HHvMzum2g',
+        prefix: '!',
+        activity: 'Num Num adlı sunucuya hizmet veriyor. ',
+    },
+
+    filters: ['8D', 'gate', 'haas', 'phaser', 'treble', 'tremolo', 'vibrato', 'reverse', 'karaoke', 'flanger', 'mcompand', 'pulsator', 'subboost', 'bassboost', 'vaporwave', 'nightcore', 'normalizer', 'surrounding'],
+};
